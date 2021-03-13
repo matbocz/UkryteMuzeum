7c507971ed6a2720a325484acea48cba2460afe7
Mateusz Boczarski <matbocz@outlook.com>
2021-03-13 15:55:41 +0100

    Add ability to looking

---
+++ Assets/Scripts/PlayerLooking.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLooking : MonoBehaviour
+{
+    [Header("Looking")]
+    [SerializeField] float sensitivityX = 60f;
+    [SerializeField] float sensitivityY = 60f;
+    [SerializeField] float rotationMultiplier = 0.02f;
+
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        CameraSetup();
+    }
+
+    private void CameraSetup()
+    {
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        MyInput();
+        ProcessRotation();
+    }
+
+    private void MyInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensitivityX * rotationMultiplier;
+        xRotation -= mouseY * sensitivityY * rotationMultiplier;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+
+    private void ProcessRotation()
+    {
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
