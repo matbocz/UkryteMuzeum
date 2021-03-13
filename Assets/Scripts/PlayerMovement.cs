912b561e68f7bb8a1707463cdec036a60c813bcd
Mateusz Boczarski <matbocz@outlook.com>
2021-03-13 14:49:34 +0100

    Create WSAD movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float moveMultiplier = 10f;
+    [SerializeField] float bodyDrag = 8f;
+
+    float horizontalMove;
+    float verticalMove;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+        ControlDrag();
+    }
+
+    private void MyInput()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * verticalMove + transform.right * horizontalMove;
+    }
+
+    private void ControlDrag()
+    {
+        rb.drag = bodyDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        rb.AddForce(moveDirection.normalized * moveSpeed * moveMultiplier, ForceMode.Acceleration);
+    }
+}
