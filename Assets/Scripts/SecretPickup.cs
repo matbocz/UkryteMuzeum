d9b69d475f015af01f90042bf48a151c34f45b7f
Mateusz Boczarski <matbocz@outlook.com>
2021-03-16 18:44:04 +0100

    Create SecretPickup script

---
+++ Assets/Scripts/SecretPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Secret!");
+            //Destroy(gameObject);
+        }
+    }
+}
