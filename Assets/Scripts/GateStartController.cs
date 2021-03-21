c0981718db9d557ed0e8de930c7012a910cd56e6
SAJGONNDG <65765028+SAJGONNDG@users.noreply.github.com>
2021-03-21 13:46:41 +0100

    SmallChanges
    Upadate 21.03

---
+++ Assets/Scripts/GateStartController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateStartController : MonoBehaviour
+{
+    Animator gateStart;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gateStart.SetBool("isOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        gateStart.SetBool("isOpening", false);
+    }
+
+    private void Start()
+    {
+        gateStart = this.transform.parent.GetComponent<Animator>();
+    }
+}
