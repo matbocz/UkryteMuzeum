517a1e9f224680044436ff42f75db000d385fc66
Mateusz Boczarski <matbocz@outlook.com>
2021-04-21 11:21:27 +0200

    Create scripts to handle quiz
    - QuizManager
    - QuestionManager

---
+++ Assets/Scripts/Quiz/QuestionManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class QuestionManager : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] private Button[] questionButtons = new Button[4];
+
+    [Space(10)]
+    [SerializeField] private Button correctButton;
+    [SerializeField] private Button nextQuestionButton;
+
+    private void Start()
+    {
+        nextQuestionButton.gameObject.SetActive(false);
+    }
+
+    public void AnswerQuestion()
+    {
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+
+        ColorBlock clickedButtonColors = clickedButton.GetComponent<Button>().colors;
+        ColorBlock correctButtonColors = correctButton.colors;
+
+        DisableAllQuestionButtons();
+
+        if (clickedButton.name == correctButton.name)
+        {
+            HandleGoodAnswer(clickedButton, clickedButtonColors);
+        }
+        else
+        {
+            HandleBadAnswer(clickedButton, clickedButtonColors, correctButtonColors);
+        }
+
+        nextQuestionButton.gameObject.SetActive(true);
+    }
+
+    private void DisableAllQuestionButtons()
+    {
+        foreach (Button button in questionButtons)
+        {
+            button.enabled = false;
+        }
+    }
+
+    private void HandleGoodAnswer(GameObject clickedButton, ColorBlock clickedButtonColors)
+    {
+        Debug.Log("Correct");
+
+        QuizManager.instance.IncreasePoints();
+
+        clickedButtonColors.normalColor = Color.green;
+
+        clickedButton.GetComponent<Button>().colors = clickedButtonColors;
+    }
+
+    private void HandleBadAnswer(GameObject clickedButton, ColorBlock clickedButtonColors, ColorBlock correctButtonColors)
+    {
+        Debug.Log("Incorrect");
+
+        clickedButtonColors.normalColor = Color.red;
+        correctButtonColors.normalColor = Color.green;
+
+        clickedButton.GetComponent<Button>().colors = clickedButtonColors;
+        correctButton.colors = correctButtonColors;
+    }
+}
---
+++ Assets/Scripts/Quiz/QuizManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject startPanel;
+    [SerializeField] private GameObject endPanel;
+
+    [Space(10)]
+    [SerializeField] private GameObject[] questionPanels = new GameObject[5];
+
+    private int questionNumber = 1;
+    private int points = 0;
+
+    public static QuizManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        GameStateManager.instance.ShowCursor();
+
+        startPanel.SetActive(true);
+    }
+
+    public void StartQuiz()
+    {
+        startPanel.SetActive(false);
+        questionPanels[0].SetActive(true);
+    }
+
+    public void ShowNextQuestion()
+    {
+        questionPanels[questionNumber - 1].SetActive(false);
+        questionPanels[questionNumber].SetActive(true);
+
+        questionNumber++;
+    }
+
+    public void ShowResults()
+    {
+        questionPanels[questionNumber - 1].SetActive(false);
+        endPanel.SetActive(true);
+
+        endPanel.GetComponentInChildren<Text>().text = "Wyniki: " + GetPoints() + " / " + GetNumberOfQuestions();
+    }
+
+    private int GetPoints()
+    {
+        return points;
+    }
+
+    private int GetNumberOfQuestions()
+    {
+        return questionPanels.Length;
+    }
+
+    public void EndQuiz()
+    {
+        Debug.Log("End game");
+        Application.Quit();
+    }
+
+    public void IncreasePoints()
+    {
+        points++;
+    }
+}
