f724a98d3f2d82e8cd5a335ce15bdb495bf0ed55
SAJGONNDG <65765028+SAJGONNDG@users.noreply.github.com>
2021-05-07 16:00:45 +0200

    SmallChangesAndNewThings

---
+++ Assets/Scripts/GameCubeEnd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCubeEnd : MonoBehaviour
+{
+    Animator gameCube;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameCube.SetBool("isEnd", true);
+    }
+
+    private void Start()
+    {
+        gameCube = this.transform.parent.GetComponent<Animator>();
+    }
+}
